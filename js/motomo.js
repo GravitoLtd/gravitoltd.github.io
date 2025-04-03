7e946628b6c2a31bb8ef60af2ea3d4996cec41e3
Unnikrishnan Kurup <unni@gravito.net>
2025-04-03 14:43:53 +0300

    Deployed 4f7d1e8 with MkDocs version: 1.6.1

--- js/motomo.js
+++ js/motomo.js
@@ -1,7 +1,12 @@
 
-  var _mtm = window._mtm = window._mtm || [];
-  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
   (function() {
+    var u="//analytics.gravito.net/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '7']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://analytics.gravito.net/js/container_HF7B6A2b.js'; s.parentNode.insertBefore(g,s);
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
