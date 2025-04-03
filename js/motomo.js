41415a0458efb5f881275d72c19f800a47e1d5bc
Unnikrishnan Kurup <unni@gravito.net>
2025-04-03 10:59:18 +0300

    Deployed 4f7d1e8 with MkDocs version: 1.6.1

---
+++ js/motomo.js
@@ -0,0 +1,7 @@
+
+  var _mtm = window._mtm = window._mtm || [];
+  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+  (function() {
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://analytics.gravito.net/js/container_HF7B6A2b.js'; s.parentNode.insertBefore(g,s);
+  })();
